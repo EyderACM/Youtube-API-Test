e6d93fbc5670d49b5b867e5d5b44df706d5875c2
Eyder ACM <eyderacm@gmail.com>
2018-12-25 18:28:42 -0600

    Solve bug

--- main.js
+++ main.js
@@ -124,6 +124,7 @@ function requestVideoPlaylists(playlistId) {
 
     request.execute(response => {        
         const playListItems = response.items;
+        videoContainer.innerHTML = "";
         if(playListItems) {            
             let output = `<h4 class="align-center">Latest Videos</h4><div class="videos">`;
             // Loop through videos and append output            
